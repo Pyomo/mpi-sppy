019eb8367e0f5063ebb85b2bd79a9871737364f8
David L. Woodruff <DLWoodruff@UCDavis.edu>
2020-06-05 14:45:42 -0700

    add code for quick tests of some examples

---
+++ mpisppy/examples/afew.py
@@ -0,0 +1,73 @@
+# This software is distributed under the 3-clause BSD License.
+# Run a few examples; dlw June 2020
+# See also runall.py
+# Assumes you run from the examples directory.
+# Optional command line arguments: solver_name mpiexec_arg
+# E.g. python run_all.py
+#      python run_all.py cplex
+#      python run_all.py gurobi_persistent --oversubscribe
+
+import os
+import sys
+
+solver_name = "gurobi_persistent"
+if len(sys.argv) > 1:
+    solver_name = sys.argv[1]
+
+# Use oversubscribe if your computer does not have enough cores.
+# Don't use this unless you have to.
+# (This may not be allowed on versions of mpiexec)
+mpiexec_arg = ""  # "--oversubscribe"
+if len(sys.argv) > 2:
+    mpiexec_arg = sys.argv[2]
+
+badguys = dict()
+
+def egret_avail():
+    try:
+        import egret
+    except:
+        return False
+
+    p = str(egret.__path__)
+    l = p.find("'")
+    r = p.find("'", l+1)
+    egretrootpath = p[l+1:r]
+
+    egret_thirdparty_path = os.path.join(egretrootpath, "thirdparty")
+    if os.path.exists(os.path.join(egret_thirdparty_path, "pglib-opf-master")):
+        return True
+    
+    from egret.thirdparty.get_pglib_opf import get_pglib_opf
+    get_pglib_opf(egret_thirdparty_path)
+    return True
+
+def do_one(dirname, progname, np, argstring):
+    os.chdir(dirname)
+    runstring = "mpiexec {} -np {} python -m mpi4py {} {}".\
+                format(mpiexec_arg, np, progname, argstring)
+    print(runstring)
+    code = os.system(runstring)
+    if code != 0:
+        if dirname not in badguys:
+            badguys[dirname] = [runstring]
+        else:
+            badguys[dirname].append(runstring)
+    os.chdir("..")
+
+
+# for farmer, the first arg is num_scens and is required
+do_one("farmer", "farmer_cylinders.py", 3,
+       "3 --bundles-per-rank=0 --max-iterations=50 "
+       "--default-rho=1 "
+       "--solver-name={} --no-fwph".format(solver_name))
+
+if len(badguys) > 0:
+    print("\nBad Guys:")
+    for i,v in badguys.items():
+        print("Directory={}".format(i))
+        for c in v:
+            print("    {}".format(c))
+        sys.exit(1)
+else:
+    print("\nAll OK.")
