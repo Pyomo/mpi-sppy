fe05fb8318aa5a8c4d54797066c09f932e5d3e02
David L Woodruff <dlwoodruff@ucdavis.edu>
2020-08-11 16:54:52 -0700

    clean example for sanity checking

--- paperruns/scripts/farmer/quicky.bash
+++ paperruns/scripts/farmer/quicky.bash
@@ -6,11 +6,18 @@ SNAME="cplex"
 
 LOC="../../../mpisppy/examples/farmer"
 
-CROPSMULT=1000
-SCENCNT=3
-let NP=3*${SCENCNT}
+CROPSMULT=500
+SCENCNT=6
+SCENPERBUN=2
+# use 0 for no bundles
+if [ ${SCENPERBUN} -gt 0 ]
+   then
+   let NP=3*${SCENCNT}/${SCENPERBUN}
+   else
+   let NP=3*${SCENCNT}
+fi
 
-mpiexec --oversubscribe -np ${NP} python -m mpi4py ${LOC}/farmer_cylinders.py ${SCENCNT} --max-iterations=100 --rel-gap=0.01 --with-xhatshuffle --default-rho=5 --max-solver-threads=2 --seed=${SEED} --solver-name=$SNAME --crops-mult=${CROPSMULT} --trace-prefix="f_${SCENCNT}_${SEED}_"  --no-fwph 
+mpiexec --oversubscribe -np ${NP} python -m mpi4py ${LOC}/farmer_cylinders.py ${SCENCNT} --max-iterations=100 --rel-gap=0.01 --with-xhatshuffle --default-rho=0.1 --max-solver-threads=2 --seed=${SEED} --solver-name=$SNAME --crops-mult=${CROPSMULT} --trace-prefix="f_${SCENCNT}_${SEED}_"  --no-fwph 
 
 
 
