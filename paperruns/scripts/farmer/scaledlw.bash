69a63ad61f551a2f45d4484e2c34e2a0e954fb68
David L Woodruff <dlwoodruff@ucdavis.edu>
2020-08-11 16:23:32 -0700

    restore to pre-merge

--- paperruns/scripts/farmer/scaledlw.bash
+++ paperruns/scripts/farmer/scaledlw.bash
@@ -7,16 +7,16 @@ LOC="../../../mpisppy/examples/farmer"
 
 CROPSMULT=1000
 SCENCNT=1000
-SCENPERBUN=20   # be sure this divides SCENCNT
+SCENPERBUN=50   # be sure this divides SCENCNT
 PFACT=10        # be sure this devides (SCENCNT/SCENPERBUN)
 
 
 #for PFACT in {20..1..-5}
 #  do
-    let NP=4*${PFACT}
+    let NP=3*${PFACT}
     let BPR=${SCENCNT}/${SCENPERBUN}/${PFACT}
 
 
-    mpiexec --oversubscribe -np ${NP} python -m mpi4py ${LOC}/cs_farmer.py ${SCENCNT} --bundles-per-rank=${BPR} --max-iterations=100 --rel-gap=0.01 --with-xhatshuffle --default-rho=1 --max-solver-threads=2 --seed=${SEED} --solver-name=$SNAME --crops-mult=${CROPSMULT} --trace-prefix="f_${NP}_${SCENCNT}_${SEED}_"  --no-fwph --max-solver-threads=2 >& NP_${NP}.out
+    mpiexec --oversubscribe -np ${NP} python -m mpi4py ${LOC}/farmer_cylinders.py ${SCENCNT} --bundles-per-rank=${BPR} --max-iterations=100 --rel-gap=0.01 --with-xhatshuffle --default-rho=0.1 --max-solver-threads=2 --seed=${SEED} --solver-name=$SNAME --crops-mult=${CROPSMULT} --trace-prefix="f_${NP}_${SCENCNT}_${SEED}_"  --no-fwph --max-solver-threads=2 >& NP_${NP}.out
     mv *.csv savescv
 #  done
