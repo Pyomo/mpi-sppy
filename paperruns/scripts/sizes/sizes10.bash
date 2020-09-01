4197244793caab9559fd840b58fc5997814c5bb9
David L. Woodruff <dlwoodruff@ucdavis.edu>
2020-09-01 12:52:02 -0700

    add a sizes example for hub overhead testing

---
+++ paperruns/scripts/sizes/sizes10.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SLOC=/home/woodruff/Documents/Research/DLWFORKS/mpi-sppy/mpisppy/examples/sizes/
+SNAME=gurobi_persistent
+
+###mpiexec -np 3 python ${SLOC}sizes_cylinders.py --num-scens=10 --bundles-per-rank=0 --max-iterations=500 --default-rho=1 --iter0-mipgap=0.01 --iterk-mipgap=0.001 --solver-name=${SNAME} --no-fwph --rel-gap=0.001
+
+mpiexec -np 4 python ${SLOC}sizes_cylinders.py --num-scens=10 --bundles-per-rank=0 --max-iterations=500 --default-rho=1 --iter0-mipgap=0.01 --iterk-mipgap=0.001 --solver-name=${SNAME} --with-fwph --rel-gap=0.001
