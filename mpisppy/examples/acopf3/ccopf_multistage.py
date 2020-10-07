ccdaa5e2ef3d9500947fffd0f1b040077d834281
David L Woodruff <DLWoodruff@users.noreply.github.com>
2020-10-07 15:59:09 -0700

    Minoracopf (#71)
    * double the time for the weekly test
    
    * allow easier outside call to the acopf constructor
    
    * make it easier to call from outside
    
    * make the tee work correctly for optional scenario creation solves

--- mpisppy/examples/acopf3/ccopf_multistage.py
+++ mpisppy/examples/acopf3/ccopf_multistage.py
@@ -54,7 +54,10 @@ def _md_dict(cb_data):
     l = p.find("'")
     r = p.find("'", l+1)
     egretrootpath = p[l+1:r]
-    test_case = egretrootpath+cb_data["epath"]
+    if cb_data["epath"][0] != os.sep:
+        test_case = os.path.join(egretrootpath, cb_data["epath"])
+    else:
+        test_case = os.path.join(egretrootpath, cb_data["epath"][1:])
 
     # look at egret/data/model_data.py for the format specification of md_dict
     return create_ModelData(test_case)
@@ -195,13 +198,13 @@ def pysp2_callback(scenario_name,
                 scen_model=inst, parent_name=parent_name))
             parent_name = enode.Name
     
-    inst._PySPnode_list = node_list
-    # Optionally assign probability to PySP_prob 
+    inst._PySPnode_list = node_list 
     inst.PySP_prob = 1 / etree.numscens
     # solve it so subsequent code will have a good start
     if solver is not None:
         print(f"scenario creation callback is solving {scenario_name} on rank {rank}")
-        solver.solve(inst)  #, tee=True) #symbolic_solver_labels=True, keepfiles=True)
+        tee = cb_data["tee"] if "tee" in cb_data else False
+        solver.solve(inst, tee=tee)   #symbolic_solver_labels=True, keepfiles=True)
 
     # attachments
     inst._enodes = enodes
