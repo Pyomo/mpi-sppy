cd53043e358262b9ce2f9c9849c17c8f521709c2
David L. Woodruff <DLWoodruff@UCDavis.edu>
2021-02-23 09:24:46 -0800

    clean up logic for ef with nonant_for_fixed_vars

--- mpisppy/utils/sputils.py
+++ mpisppy/utils/sputils.py
@@ -357,17 +357,11 @@ def _create_EF_from_scen_dict(scen_dict, EF_name=None,
                     ref_vars[(ndn, i)] = v
                 # Add a non-anticipativity constraint, except in the case when
                 # the variable is fixed and nonant_for_fixed_vars=False.
-                else:
-                    if (nonant_for_fixed_vars):
-                        expr = LinearExpression(linear_coefs=[1,-1],
-                                                linear_vars=[v,ref_vars[(ndn,i)]],
-                                                constant=0.)
-                        nonant_constr[(ndn,i,sname)] = (expr, 0.0)
-                    elif (not v.is_fixed()):
-                        expr = LinearExpression(linear_coefs=[1,-1],
-                                                linear_vars=[v,ref_vars[(ndn,i)]],
-                                                constant=0.)
-                        nonant_constr[(ndn,i,sname)] = (expr, 0.0)
+                elif (nonant_for_fixed_vars) or (not v.is_fixed()):
+                    expr = LinearExpression(linear_coefs=[1,-1],
+                                            linear_vars=[v,ref_vars[(ndn,i)]],
+                                            constant=0.)
+                    nonant_constr[(ndn,i,sname)] = (expr, 0.0)
 
             for i in range(nlens_ef_suppl[ndn]):
                 v = node.nonant_ef_suppl_vardata_list[i]
