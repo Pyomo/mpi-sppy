317175337942aa3c5d883acad7f31ed7a5544d83
Bernard Knueven <Bernard.Knueven@nrel.gov>
2021-05-05 16:41:22 -0600

    undoing phbase changes

--- mpisppy/phbase.py
+++ mpisppy/phbase.py
@@ -1290,11 +1290,7 @@ class PHBase(mpisppy.spbase.SPBase):
                 for sname in self.names_in_bundles[rank_local][bun]:
                     if (verbose and self.cylinder_rank==0):
                         print ("bundling "+sname+" into "+bname)
-                    # DLW April 2021 (delete this comment in June 2021)
-                    # This will cause merge conflicts with the phbase refactor.
-                    scen = self.local_scenarios[sname]
-                    sdict[sname] = scen
-                    scen._mpisppy_data.bundlename = bname
+                    sdict[sname] = self.local_scenarios[sname]
                 self.local_subproblems[bname] = self.FormEF(sdict, bname)
                 self.local_subproblems[bname].scen_list = \
                     self.names_in_bundles[rank_local][bun]
