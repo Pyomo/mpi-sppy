8f1d031b16d7decab7cc65c26b4f8d1b02288edc
sylvainczx <sylvain.czx@gmail.com>
2021-06-16 15:56:02 +0200

    Fixing afew with new branching factors command

--- examples/afew.py
+++ examples/afew.py
@@ -50,7 +50,7 @@ do_one("sizes",
        "--iter0-mipgap=0.01 --iterk-mipgap=0.001 "
        "--default-rho=1 --solver-name={} --with-display-progress".format(solver_name))
 do_one("hydro", "hydro_cylinders.py", 3,
-       "--BFs=3,3 --bundles-per-rank=0 --max-iterations=100 "
+       "--BFs 3 3 --bundles-per-rank=0 --max-iterations=100 "
        "--default-rho=1 --with-xhatspecific --with-lagrangian "
        "--solver-name={}".format(solver_name))
 
