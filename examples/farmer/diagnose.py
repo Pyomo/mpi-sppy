3e1f64ae9233f26bd955d70d7378b7f079297edb
David L Woodruff <dlwoodruff@ucdavis.edu>
2021-07-21 16:55:32 -0700

    the example for farmer seems to work

--- examples/farmer/diagnose.py
+++ examples/farmer/diagnose.py
@@ -3,10 +3,11 @@
 # Demonstrate the use of the scenario diagnostic software
 # Intended to be run "from" the farmer example directory
 
+import sys
 import farmer
 import mpisppy.utils.scenario_diagnostics as diag
 
-if __name__ == main:
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("usage python diagnose.py {crops_multiplier} {scen_count} {solver_name}")
         print("e.g., python diagnose.py 1 3 gurobi")
@@ -23,13 +24,14 @@ if __name__ == main:
         "crops_multiplier": crops_multiplier,
     }
 
-    solve_kwargs = {"Tee": True}
+    solve_kwargs = {"tee": True}
 
     scenario_names = ['Scenario' + str(i) for i in range(scen_count)]
 
     scenario_name, pyomodel = diag.find_problematic_scenario(solver_name,
+                                                             scenario_names,
                                                              scenario_creator,
-                                                             scenario_creator_kwargs,
+                                                             scenario_creator_kwargs = scenario_creator_kwargs,
                                                              solve_kwargs=solve_kwargs)
-    print(f"This should be None={None}")
+    print(f"Done. This should be None={None}")
 
