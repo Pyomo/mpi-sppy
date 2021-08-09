dc26523e5a2d7d5708d1550a590b2b5dab2eb907
sylvainczx <sylvain.czx@gmail.com>
2021-08-09 15:04:55 +0200

    Adding multistage xhatshuffle to runall

--- examples/run_all.py
+++ examples/run_all.py
@@ -188,9 +188,9 @@ do_one("hydro", "hydro_cylinders.py", 3,
        "--default-rho=1 --with-xhatspecific --with-lagrangian "
        "--solver-name={}".format(solver_name))
 do_one("hydro", "hydro_ef.py", 1, solver_name)
-do_one("aircond", "aircond_cylinders.py", 3,
+do_one("aircond", "aircond_cylinders.py", 4,
        "--BFs 4 3 2 --bundles-per-rank=0 --max-iterations=100 "
-       "--default-rho=1 --with-xhatspecific --with-lagrangian "
+       "--default-rho=1 --with-xhatspecific --with-lagrangian --with-xhat-shuffle"
        "--solver-name={}".format(solver_name))
 
 
