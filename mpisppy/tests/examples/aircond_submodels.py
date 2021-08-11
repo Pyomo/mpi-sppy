d16c99fef2ab909adcc987d396bb40134f501539
briancknight <briancknight31@gmail.com>
2021-08-10 17:36:28 -0700

    reverting aircond_submodel main

--- mpisppy/tests/examples/aircond_submodels.py
+++ mpisppy/tests/examples/aircond_submodels.py
@@ -346,25 +346,25 @@ if __name__ == "__main__":
                     }
     refmodel = "mpisppy.tests.examples.aircond_submodels" # WARNING: Change this in SPInstances
     # #We use from_module to build easily an Amalgomator object
-    ama = amalgomator.from_module(refmodel,
-                                  ama_options,use_command_line=False)
-    ama.run()
-    print(f"inner bound=", ama.best_inner_bound)
-    print(f"outer bound=", ama.best_outer_bound)
+    # ama = amalgomator.from_module(refmodel,
+    #                               ama_options,use_command_line=False)
+    # ama.run()
+    # print(f"inner bound=", ama.best_inner_bound)
+    # print(f"outer bound=", ama.best_outer_bound)
     
-    from mpisppy.confidence_intervals.mmw_ci import MMWConfidenceIntervals
-    options = ama.options
-    options['solver_options'] = options['EF_solver_options']
-    xhat = sputils.nonant_cache_from_ef(ama.ef)
+    # from mpisppy.confidence_intervals.mmw_ci import MMWConfidenceIntervals
+    # options = ama.options
+    # options['solver_options'] = options['EF_solver_options']
+    # xhat = sputils.nonant_cache_from_ef(ama.ef)
    
     
-    num_batches = 10
-    batch_size = 100
+    # num_batches = 10
+    # batch_size = 100
     
-    mmw = MMWConfidenceIntervals(refmodel, options, xhat, num_batches,batch_size=batch_size,
-                        verbose=False)
-    r=mmw.run(objective_gap=True)
-    print(r)
+    # mmw = MMWConfidenceIntervals(refmodel, options, xhat, num_batches,batch_size=batch_size,
+    #                     verbose=False)
+    # r=mmw.run(objective_gap=True)
+    # print(r)
     
     #An example of sequential sampling for the aircond model
     from mpisppy.confidence_intervals.seqsampling import SeqSampling
