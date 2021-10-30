f94d40c6555bd72c2793d90b036ae404701d8b99
Dave Woodruff <dlwoodruff@ucdavis.edu>
2021-10-30 10:30:52 -0700

    add a bash for aircond mmw (multi-stage)

---
+++ examples/aircond/aircond_mmw.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOLVERNAME="cplex"
+
+# get an xhat
+# xhat output file name is hardwired
+mpiexec --oversubscribe -np 3 python -m mpi4py aircond_cylinders.py --bundles-per-rank=0 --max-iterations=50 --default-rho=1 --solver-name=${SOLVERNAME} --branching-factors 4 3 2 
+
+echo "starting zhat4xhat"
+# NOTE: num_scens is restricted by the availability of data directories
+python -m mpisppy.confidence_intervals.zhat4xhat aaircond aircond_cyl_nonants.npy --solver-name ${SOLVERNAME} --branching-factors 4 3 2 --num-samples 5 --confidence-level 0.95
