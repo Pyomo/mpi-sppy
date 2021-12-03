7d1e0691f73448979b74bc0a37efdfa955c24ee7
Dave Woodruff <dlwoodruff@ucdavis.edu>
2021-12-02 18:36:42 -0800

    drop tests that depend on an obsolete docker image and weren't really valuable

--- examples/aircond/aircond_zhat.bash
+++ examples/aircond/aircond_zhat.bash
@@ -7,5 +7,4 @@ SOLVERNAME="cplex"
 mpiexec --oversubscribe -np 3 python -m mpi4py aircond_cylinders.py  --branching-factors 4 3 2 --bundles-per-rank=0 --max-iterations=50 --default-rho=1 --solver-name=${SOLVERNAME}
 
 echo "starting zhat4xhat"
-# NOTE: num_scens is restricted by the availability of data directories
 python -m mpisppy.confidence_intervals.zhat4xhat aaircond aircond_cyl_nonants.npy --solver-name ${SOLVERNAME} --branching-factors 4 3 2 --num-samples 5 --confidence-level 0.95
