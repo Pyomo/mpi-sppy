41cc071b0aed56d02aa04d1581693ec3eaf6f65d
Watson <watson61@ml-9582789.llnl.gov>
2021-12-14 12:33:27 -0800

    Adding keywords to allow for propagation of ph converger

--- mpisppy/utils/vanilla.py
+++ mpisppy/utils/vanilla.py
@@ -77,6 +77,7 @@ def ph_hub(
     all_scenario_names,
     scenario_creator_kwargs=None,
     ph_extensions=None,
+    ph_converger=None,
     rho_setter=None,
     variable_probability=None,
     all_nodenames=None,
@@ -110,6 +111,7 @@ def ph_hub(
             "rho_setter": rho_setter,
             "variable_probability": variable_probability,
             "extensions": ph_extensions,
+            "PH_converger": ph_converger,
             "all_nodenames": all_nodenames
         }
     }
