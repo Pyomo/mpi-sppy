700fa51b3595ca2bae0dba192cb2c4dac7b3bbcf
Matthew-Signorotti <Matthew-Signorotti@users.noreply.github.com>
2022-02-04 14:07:39 -0800

    Switch from create_EF to mpi-sppy's recommended ExtensiveForm class

--- examples/usar/extensive_form.py
+++ examples/usar/extensive_form.py
@@ -1,17 +1,13 @@
 import itertools
 
-import pyomo.environ as pyo
-
 from generate_data import generate_data
-import mpisppy.utils.sputils
+import mpisppy.opt.ef
 from parser import new_parser
 from scenario_creator import scenario_creator
 
 
 def main() -> None:
     args = new_parser().parse_args()
-
-    solver = pyo.SolverFactory(args.solver_name)
 
     if args.num_scens <= 0:
         raise ValueError("Provide a positive integer for num_scens")
@@ -20,13 +16,15 @@ def main() -> None:
     data_dicts = list(
         itertools.islice(generate_data(**vars(args)), args.num_scens))
 
-    ef = mpisppy.utils.sputils.create_EF(
+    ef = mpisppy.opt.ef.ExtensiveForm(
+        {"solver": args.solver_name},
         scenario_names,
         scenario_creator,
         scenario_creator_kwargs={"data_dicts": data_dicts},
+        model_name="USAR extensive form",
     )
 
-    solver.solve(ef, tee=True)
+    ef.solve_extensive_form(tee=True)
 
 
 if __name__ == "__main__":
