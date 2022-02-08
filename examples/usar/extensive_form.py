c02b53ee5a4865f51493d64eb53640948ccd9236
Matthew-Signorotti <Matthew-Signorotti@users.noreply.github.com>
2022-02-07 19:43:58 -0800

    Add extensive form function which does not read command-line args

--- examples/usar/extensive_form.py
+++ examples/usar/extensive_form.py
@@ -6,23 +6,29 @@ from parser import new_parser
 from scenario_creator import scenario_creator
 
 
-def main() -> None:
-    args = new_parser().parse_args()
-
-    if args.num_scens <= 0:
+def extensive_form(
+    solver_name: str, num_scens: int, **generate_data_kwargs
+) -> mpisppy.opt.ef.ExtensiveForm:
+    if num_scens <= 0:
         raise ValueError("Provide a positive integer for num_scens")
-    scenario_names = list(map(str, range(args.num_scens)))
+    scenario_names = list(map(str, range(num_scens)))
 
     data_dicts = list(
-        itertools.islice(generate_data(**vars(args)), args.num_scens))
+        itertools.islice(generate_data(**generate_data_kwargs), num_scens))
 
-    ef = mpisppy.opt.ef.ExtensiveForm(
-        {"solver": args.solver_name},
+    return mpisppy.opt.ef.ExtensiveForm(
+        {"solver": solver_name},
         scenario_names,
         scenario_creator,
         scenario_creator_kwargs={"data_dicts": data_dicts},
         model_name="USAR extensive form",
     )
+
+
+def main() -> None:
+    args = new_parser().parse_args()
+
+    ef = extensive_form(**vars(args))
 
     ef.solve_extensive_form(tee=True)
 
