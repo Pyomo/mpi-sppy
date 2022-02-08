2dd7db4081185a22d7f765f891b25753c0dd3d8f
bknueven <30801372+bknueven@users.noreply.github.com>
2022-02-08 15:17:53 -0700

    Trying multistage FWPH (#205)
    * trying multistage FWPH
    
    * Use FWPH in straight_tests.py
    
    * prepping for merge

---
+++ examples/aircond/fwdemo.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TBD: aircond uses start-seed (but seed is allowed as an arg that is ignore).
+
+SOLVERNAME="xpress_persistent"
+
+# get an xhat 
+# xhat output file name is hardwired
+mpiexec --oversubscribe -np 10 python -m mpi4py aircond_cylinders.py --bundles-per-rank=0 --max-iterations=1000 --default-rho=0.1 --solver-name=${SOLVERNAME} --branching-factors 4 3 2 --Capacity 200 --QuadShortCoeff 0.3  --BeginInventory 50 --rel-gap -0.01 --mu-dev 0 --sigma-dev 40 --max-solver-threads 1 --start-seed 0 --with-fwph --start-ups --iter0-mipgap=0.0 --intra-hub-conv-thresh=-0.01 --max-stalled-iters=500
--- mpisppy/fwph/fwph.py
+++ mpisppy/fwph/fwph.py
@@ -42,6 +42,7 @@ import numpy as np
 import pyomo.environ as pyo
 import time
 import re # For manipulating scenario names
+from mpisppy import global_toc
 
 from mpi4py import MPI
 from pyomo.repn.standard_repn import generate_standard_repn
@@ -89,7 +90,6 @@ class FWPH(mpisppy.phbase.PHBase):
 
     def fw_prep(self):
         self.PH_Prep(attach_duals=True, attach_prox=False)
-        self._check_for_multistage()
         self.subproblem_creation(self.options['verbose'])
         self._output_header()
 
@@ -433,11 +433,6 @@ class FWPH(mpisppy.phbase.PHBase):
                 mip.leaf_vars = { ('LEAF', ix):
                     var for ix, var in enumerate(self._get_leaf_vars(mip))
                 }
-
-    def _check_for_multistage(self):
-        if self.multistage:
-            raise RuntimeError('The FWPH algorithm only supports '
-                               'two-stage models at this time.')
 
     def _check_initial_points(self):
         ''' If t_max (i.e. the inner iteration limit) is set to 1, then the
