c55a4dbf9e4fadac0abfdd36fd0571bce264db40
David L Woodruff <DLWoodruff@users.noreply.github.com>
2022-02-08 14:27:40 -0800

    Shiva dlw (#215)
    * a slurm script that works on shiva
    
    * react to changes in default cylinders
    
    * a one node slurm script (could use fwph)
    
    * slurm script
    
    * enable FWPH
    
    * removing a file that should be there

--- examples/aircond/aircond_cylinders.py
+++ examples/aircond/aircond_cylinders.py
@@ -206,7 +206,6 @@ def main():
                                                   scenario_creator_kwargs=scenario_creator_kwargs,
                                                   rho_setter = primal_rho_setter,
                                                   all_nodenames = all_nodenames)
-
     # Indepdent FWPH bound spoke
     if with_fwph:
         fwph_spoke = vanilla.fwph_spoke(*beans,
