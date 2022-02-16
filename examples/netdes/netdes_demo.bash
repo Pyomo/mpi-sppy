206c4591903fc1b2deb40291c6f1b3a2ae10e885
bknueven <30801372+bknueven@users.noreply.github.com>
2022-02-16 16:59:23 -0700

    adding netdes demo (#223)

---
+++ examples/netdes/netdes_demo.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SOLVER=xpress_persistent
+
+mpirun -n 6 python netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=2 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=0.01
