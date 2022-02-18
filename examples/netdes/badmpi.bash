5aa098ee8760d00e181dbe768b68de7a64fa86cb
David L Woodruff <dlwoodruff@ucdavis.edu>
2022-02-17 18:30:18 -0800

    make the bash scipts work as bash scripts without editing

--- examples/netdes/badmpi.bash
+++ examples/netdes/badmpi.bash
@@ -11,8 +11,8 @@
 #SBATCH --nodelist=c[4]
 
 SOLVER=gurobi_persistent
-export GRB_LICENSE_FILE=/home/dlwoodruff/software/gurobi950/licenses/c4/gurobi.lic
+#export GRB_LICENSE_FILE=/home/dlwoodruff/software/gurobi950/licenses/c4/gurobi.lic
 
-
+SLURM_NTASKS=10
 mpiexec -np $SLURM_NTASKS python netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=2 --default-rho=5000.0 --instance-name=network-10-20-L-02 --max-iterations=100 --rel-gap=0.01 --no-cross-scenario-cuts
 
