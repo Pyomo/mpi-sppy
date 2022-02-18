fc141e5ebf539cc41d93c601a6ae209e8ef5b26c
David L. Woodruff <dlwoodruff@ucdavis.edu>
2022-02-17 16:07:53 -0800

    demonstrate troubles

---
+++ examples/netdes/badmpi.bash
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+# This can also be run as a straight bash script
+
+#!/bin/bash -l
+#SBATCH --job-name=netdes_demo_slurm
+#SBATCH --output=netdes_demo_slurm.out
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=2
+#SBATCH --time=0-0:10:00
+#SBATCH --nodelist=c[4]
+
+SOLVER=gurobi_persistent
+export GRB_LICENSE_FILE=/home/dlwoodruff/software/gurobi950/licenses/c4/gurobi.lic
+
+
+mpiexec -np $SLURM_NTASKS python netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=2 --default-rho=5000.0 --instance-name=network-10-20-L-02 --max-iterations=100 --rel-gap=0.01 --no-cross-scenario-cuts
+
