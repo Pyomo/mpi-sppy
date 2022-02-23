512fbcd690066e4d64fda472644e7e4126194ba0
Matthew-Signorotti <Matthew-Signorotti@users.noreply.github.com>
2022-02-23 13:57:06 -0800

    Add depot and household coordinates to scenario models

--- examples/usar/extensive_form.py
+++ examples/usar/extensive_form.py
@@ -1,6 +1,6 @@
 import itertools
 
-from generate_data import generate_data
+from generate_data import generate_coords, generate_data
 import mpisppy.opt.ef
 from parser import new_parser
 from scenario_creator import scenario_creator
@@ -15,12 +15,18 @@ def extensive_form(
 
     data_dicts = list(
         itertools.islice(generate_data(**generate_data_kwargs), num_scens))
+    depot_coords, site_coords = generate_coords(**generate_data_kwargs)
+    scenario_creator_kwargs = {
+        "data_dicts": data_dicts,
+        "depot_coords": depot_coords,
+        "site_coords": site_coords,
+    }
 
     return mpisppy.opt.ef.ExtensiveForm(
         {"solver": solver_name},
         scenario_names,
         scenario_creator,
-        scenario_creator_kwargs={"data_dicts": data_dicts},
+        scenario_creator_kwargs=scenario_creator_kwargs,
         model_name="USAR extensive form",
     )
 
