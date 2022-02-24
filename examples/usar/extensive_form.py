6069f121364f3e8f61d1c9938dff2f79881ef78b
Matthew-Signorotti <Matthew-Signorotti@users.noreply.github.com>
2022-02-24 11:13:13 -0800

    Use solution writers to generate walks and Gantt plots

--- examples/usar/extensive_form.py
+++ examples/usar/extensive_form.py
@@ -1,9 +1,11 @@
 import itertools
+import os
 
 from generate_data import generate_coords, generate_data
 import mpisppy.opt.ef
 from parser import new_parser
 from scenario_creator import scenario_creator
+from write_solutions import walks_writer, gantt_writer
 
 
 def extensive_form(
@@ -37,6 +39,10 @@ def main() -> None:
     ef = extensive_form(**vars(args))
 
     ef.solve_extensive_form(tee=True)
+
+    output_dir = args.output_dir
+    ef.write_tree_solution(os.path.join(output_dir, "walks"), walks_writer)
+    ef.write_tree_solution(os.path.join(output_dir, "gantts"), gantt_writer)
 
 
 if __name__ == "__main__":
---
+++ examples/usar/write_solutions.py
@@ -0,0 +1,22 @@
+import os
+
+import matplotlib.pyplot as plt
+import pyomo.environ as pyo
+
+from plot import plot_walks, plot_gantt
+
+
+def walks_writer(
+    walks_dir: str, scen_name: str, scen_mod: pyo.ConcreteModel, bundling: bool
+) -> None:
+    plot_walks(scen_mod)
+    plt.savefig(os.path.join(walks_dir, scen_name + ".pdf"))
+    plt.close()
+
+
+def gantt_writer(
+    gantt_dir: str, scen_name: str, scen_mod: pyo.ConcreteModel, bundling: bool
+) -> None:
+    plot_gantt(scen_mod)
+    plt.savefig(os.path.join(gantt_dir, scen_name + ".pdf"))
+    plt.close()
