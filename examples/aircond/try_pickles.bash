69eb51397c785d6351418c20101abd0d3c2980f3
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-03-04 12:10:19 -0800

    try_pickles now has a good example

--- examples/aircond/try_pickles.bash
+++ examples/aircond/try_pickles.bash
@@ -6,28 +6,29 @@
 
 SOLVERNAME="cplex"
 
-BF1=2
+BF1=50
 BF2=2
-let SPB=BF2
-let SC=BF1*BF2
-let PBF=BF1
+BF3=2
+let SPB=BF2*BF3
+let SC=BF1*BF2*BF3
+let PBF=BF1  # by design
 
-BI=150
+BI=50
 NC=1
 QSC=0.3
-SD=80
-OTC=1.5
+SD=40
+OTC=5
 
 EC="--Capacity 200 --QuadShortCoeff $QSC  --BeginInventory $BI --mu-dev 0 --sigma-dev $SD --start-seed 0 --NegInventoryCost=$NC --OvertimeProdCost=$OTC"
 # --start-ups
 
 echo "^^^^^^^^^ Make pickle bundles"
-python bundle_pickler.py --branching-factors $BF1 $BF2 --pickle-bundles-dir="." --scenarios-per-bundle=$SPB $EC
+python bundle_pickler.py --branching-factors $BF1 $BF2 $BF3 --pickle-bundles-dir="." --scenarios-per-bundle=$SPB $EC
 
 
 echo "***** Use pickle bundles"
 # It is entirely up to the user to make sure that the scenario count and scenarios per bundle match between creating the pickles and using them (the costs probably don't matter, since the pickle has it all)
-mpiexec --oversubscribe -np 3 python -m mpi4py aircond_cylinders.py --max-iterations=10 --default-rho=1 --solver-name=${SOLVERNAME} --branching-factors $PBF --rel-gap 0.01 --max-solver-threads 2 --start-seed 0 --no-fwph --no-lagranger --bundles-per-rank=0  --scenarios-per-bundle=$SPB --write-solution --intra-hub-conv-thresh 0 --unpickle-bundles-dir="." $EC
+mpiexec --oversubscribe -np 3 python -m mpi4py aircond_cylinders.py --max-iterations=10 --default-rho=1 --solver-name=${SOLVERNAME} --branching-factors $PBF --rel-gap 0.001 --max-solver-threads 2 --start-seed 0 --no-fwph --no-lagranger --bundles-per-rank=0  --scenarios-per-bundle=$SPB --write-solution --intra-hub-conv-thresh 0 --unpickle-bundles-dir="." $EC
 #--with-display-convergence-detail
 
 ###python aircond_cylinders.py --help
