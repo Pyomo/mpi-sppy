4e23befb690dbb29359e70326985046590109553
David L Woodruff <DLWoodruff@users.noreply.github.com>
2022-04-07 09:00:16 -0700

    Readthedocs (#239)
    * trying to get mpi-sppy installed during readthedocs builds
    
    * add mpi4py to setup.py requirements; pyomo>=6.2
    
    * getting ready for a mock mpi4py
    
    * ready for a mock mpi4py for readthedocs
    
    * trying to work around find_packages() based on a theory...
    
    * add a cd to the setup for mock_mpisppy
    
    * merge

--- mock_mpi4py/setup.py
+++ mock_mpi4py/setup.py
@@ -2,19 +2,21 @@
 
 # setup.py for the mock mpi4py; this should be run only under very special circumstances.
 
-#from setuptools import find_packages
+import os
+
+import setuptools  # this *is* necessary
 from distutils.core import setup
 
-#packages = find_packages()
+os.chdir("mock_mpi4py")
 
 setup(
     name='mpi4py',
-    version='0.0.1',
+    version='99.99.99',
     description="mock mpi4py",
     url='https://github.com/Pyomo/mpi-sppy',
     author='David Woodruff',
     author_email='dlwoodruff@ucdavis.edu',
-
     packages=['mpi4py'],   #  packages,
     install_requires=[]
 )
+os.chdir("..")  # probably not needed
