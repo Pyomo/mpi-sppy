732d58f1332f91cc70e8e449c58c31eaba0b2cf8
David L Woodruff <DLWoodruff@users.noreply.github.com>
2022-04-18 16:38:39 -0700

    add a comma to a comment

--- mpisppy/utils/vanilla.py
+++ mpisppy/utils/vanilla.py
@@ -1,4 +1,4 @@
-# If possible use cfg_vanilla and config.py instead of this and baseparsers.py.
+# If possible, use cfg_vanilla and config.py instead of this and baseparsers.py.
 # Copyright 2020 by B. Knueven, D. Mildebrath, C. Muir, J-P Watson, and D.L. Woodruff
 # This software is distributed under the 3-clause BSD License.
 """ Plain versions of dictionaries that can be modified for each example
