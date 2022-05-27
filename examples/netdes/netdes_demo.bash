9602b2298e99df7e788330687032964244e46c7b
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-05-26 19:02:22 -0700

    netdes_demo.bash works, but I dont like (upper bounds are sporadic; there may be extensions working?)

--- examples/netdes/netdes_demo.bash
+++ examples/netdes/netdes_demo.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-SOLVER=xpress_persistent
+#SOLVER=xpress_persistent
+SOLVER=cplex
 
-mpirun -n 6 python netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=2 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=0.01
+mpiexec --oversubscribe -n 6 python netdes_cylinders.py --solver-name=${SOLVER} --max-solver-threads=2 --default-rho=10000.0 --instance-name=network-10-20-L-01 --max-iterations=100 --rel-gap=0.01 --lagrangian --xhatshuffle
