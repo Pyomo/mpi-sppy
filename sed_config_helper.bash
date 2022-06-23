840b60e5a6d61a76f7cad42f49fb0fe47a9ece1a
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-06-23 16:08:05 -0700

    update the sed bash (without testing)

--- sed_config_helper.bash
+++ sed_config_helper.bash
@@ -12,11 +12,11 @@ cp ${1} ${2}
 
 sed -i 's/import argparse/from mpisppy\.utils import config/g' ${2}
 
-sed -i 's/parser\.add_argument("--/config.add_to_config("/g' ${2}
+sed -i 's/parser\.add_argument("--/cfg.add_to_config("/g' ${2}
 # in case the first one did not hit; leave them with a bit of a mess...
-sed -i 's/iparser\.add_argument/config.add_to_config/g' ${2}
+sed -i 's/iparser\.add_argument/cfg.add_to_config/g' ${2}
 
-sed -i 's/parser\.add_argument/config.add_to_config/g' ${2}
+sed -i 's/parser\.add_argument/cfg.add_to_config/g' ${2}
 
 # note: you still need to change the - to _ inside arg names
 sed -i '/add_to_config/s/-/_/g' ${2}
@@ -36,8 +36,8 @@ sed -i "s/action ='store_true'/domain=bool,\n            default=False/g" ${2}
 sed -i '/parser\.set_defaults/d' ${2}
 
 # The config setup functions do not take arguments
-sed -i "/config/s/(parser)/()/g" ${2}
-sed -i "/config/s/(inparser)/()/g" ${2}
+sed -i "/cfg/s/(parser)/()/g" ${2}
+sed -i "/cfg/s/(inparser)/()/g" ${2}
 
 # use cfg in beans
 sed -i '/bean/s/args/cfg/g' ${2}
@@ -46,10 +46,10 @@ sed -i 's/args\./cfg\./g' ${2}
 
 # (note that " and ' are not the same for bash. You cannot evaluate ${1} inside single quotes)
 # The next two commands might make a mess with actually parsing, but might be better than doing nothing
-sed -i "s/args = parser\.parse_args()/parser = config\.create_parser(\'${1}\')\n    args = parser\.parse_args()\n    args = config\.global_config\.import_argparse(args)/g" ${2}
+sed -i "s/args = parser\.parse_args()/cfg\.parse_command_line(${1})/g" ${2}
 
 # if you have a function that sets up the parser, this might almost do the job (except you need to fix the caller; and this might be redundant...)
-sed -i "s/return parser/parser = config\.create_parser('${1}')\n    args = parser\.parse_args()\n    args = config\.global_config\.import_argparse(args)/g" ${2}
+sed -i "s/return parser/cfg\.parse_command_line(${1})/g" ${2}
 
 # we should not be using argparse to create the parser
 sed -i '/argparse\.ArgumentParser/d' ${2}
@@ -59,4 +59,4 @@ echo "You probably still need to edit (e.g., look at create_parser and lines nea
 echo "  ...and there may be more issues associated with parsers; see farmer_cylinders.py for an example"
 echo "    ...and check your boolean command line args"
 echo
-echo "And you probably need cfg = config.global_config in scopes where cfg is used"
+echo "And you probably need cfg = config.Config() in __main__ or someplace high up; and you will need cfg passed in wherever else cfg is used"
