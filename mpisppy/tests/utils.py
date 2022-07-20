d128afd976f4fd9f53df71fca69906fba3cc712b
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-07-20 15:01:17 -0700

    try using xpress as the solver

--- mpisppy/tests/utils.py
+++ mpisppy/tests/utils.py
@@ -6,7 +6,7 @@ import pyomo.environ as pyo
 from math import log10, floor
 
 def get_solver():
-    solvers = [n+e for e in ('_persistent', '') for n in ("cplex","gurobi","xpress")]
+    solvers = [n+e for e in ('_persistent', '') for n in ("xpress","cplex","gurobi")]
     
     for solvername in solvers:
         try:
