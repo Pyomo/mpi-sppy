148ceb65ec2716050fd4a99a027da76b01f4c791
Dave Woodruff <dlwoodruff@ucdavis.edu>
2022-12-12 10:25:52 -0800

    fix a bug in farmer.py

--- examples/farmer/farmer.py
+++ examples/farmer/farmer.py
@@ -253,7 +253,7 @@ def inparser_adder(cfg):
 #=========
 def kw_creator(cfg):
     # (for Amalgamator): linked to the scenario_creator and inparser_adder
-    kwargs = {"use_integer": cfg.get('use_integer', False),
+    kwargs = {"use_integer": cfg.get('farmer_with_integers', False),
               "crops_multiplier": cfg.get('crops_multiplier', 1),
               "num_scens" : cfg.get('num_scens', None),
               }
