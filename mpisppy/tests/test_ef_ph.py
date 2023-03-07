c1397bbdd93f4c5cdce7137d64aec48c00636684
Dave Woodruff <dlwoodruff@ucdavis.edu>
2023-03-07 15:36:28 -0800

    smoke test for wtracker at least

--- mpisppy/tests/test_ef_ph.py
+++ mpisppy/tests/test_ef_ph.py
@@ -347,6 +347,29 @@ class Test_sizes(unittest.TestCase):
 
     @unittest.skipIf(not solver_available,
                      "no solver is available")
+    def test_wtracker_extension(self):
+        """ Make sure the wtracker at least does not cause a stack trace
+        """
+        from mpisppy.extensions.wtracker_extension import Wtracker_extension
+        options = self._copy_of_base_options()
+        options["PHIterLimit"] = 4
+        options["wtracker_options"] ={"wlen": 3,
+                                      "reportlen": 6,
+                                      "stdevthresh": 0.1}
+
+        ph = mpisppy.opt.ph.PH(
+            options,
+            self.all3_scenario_names,
+            scenario_creator,
+            scenario_denouement,
+            scenario_creator_kwargs={"scenario_count": 3},
+            extensions=Wtracker_extension,
+        )
+        conv, basic_obj, tbound = ph.ph_main()
+
+
+    @unittest.skipIf(not solver_available,
+                     "no solver is available")
     def test_fix_xhat_extension(self):
         """ Make sure that ph and xhat do not unfix a fixed Var
         """
