0d4e80942f7e3fa4e0b0ba2c3814513ca86bb203
Ulysse Naepels <ulysse.naepels@polytechnique.edu>
2023-07-31 09:56:16 -0700

    add new optional callback for extensions

--- mpisppy/phbase.py
+++ mpisppy/phbase.py
@@ -780,11 +780,14 @@ class PHBase(mpisppy.spopt.SPOpt):
 
         #global_toc('Rank: {} - assigning rho'.format(rank), True)
 
+        if have_extensions:
+            self.extobject.post_iter0()
+
         if self.spcomm is not None:
             self.spcomm.sync()
 
-        if have_extensions:
-            self.extobject.post_iter0()
+        if have_extensions and getattr(self.extobject, 'enditer_after_sync', None) is not None:
+            self.extobject.enditer_after_sync()
 
         if self.rho_setter is not None:
             if self.cylinder_rank == 0:
@@ -893,14 +896,17 @@ class PHBase(mpisppy.spopt.SPOpt):
                 verbose=verbose
             )
 
+            if have_extensions:
+                self.extobject.enditer()
+
             if self.spcomm is not None:
                 self.spcomm.sync()
                 if self.spcomm.is_converged():
                     global_toc("Cylinder convergence", self.cylinder_rank == 0)
                     break
 
-            if have_extensions:
-                self.extobject.enditer()
+            if have_extensions and getattr(self.extobject, 'enditer_after_sync', None) is not None:
+                self.extobject.enditer_after_sync()
 
             if dprogress and self.cylinder_rank == 0:
                 print("")
