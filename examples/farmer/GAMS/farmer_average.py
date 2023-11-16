2424686610f02b40a0134bf3515b8831ec349b00
Bernard Knueven <Bernard.Knueven@nrel.gov>
2023-11-16 11:06:42 -0800

    no gamspy

--- examples/farmer/GAMS/farmer_average.py
+++ examples/farmer/GAMS/farmer_average.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import gams
-import gamspy.utils as utils
+import gamspy_base
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
-w = gams.GamsWorkspace(working_directory=this_dir, system_directory=utils._getGAMSPyBaseDirectory())
+gamspy_base_dir = gamspy_base.__path__[0]
+
+w = gams.GamsWorkspace(working_directory=this_dir, system_directory=gamspy_base_dir)
 
 model = w.add_job_from_file("farmer_average.gms")
 
