0f3a7776bbf1fb17dd1d3c56e34c3261578fb5c9
David L. Woodruff <dlwoodruff@ucdavis.edu>
2023-12-29 13:45:04 -0800

    The last commit message was misleading: some levels are somehow bad even though the solve is good; farmer_linear_augmented.py now illustrates (without Pyomo or mpisppy)

--- examples/farmer/GAMS/farmer_linear_augmented.py
+++ examples/farmer/GAMS/farmer_linear_augmented.py
@@ -1,14 +1,112 @@
+# to assist with debugging
 import os
 import sys
 import gams
 import gamspy_base
 
-this_dir = os.path.dirname(os.path.abspath(__file__))
+def create_model(scennum = 2):
+    # create a model and return a dictionary describing it
 
-gamspy_base_dir = gamspy_base.__path__[0]
+    this_dir = os.path.dirname(os.path.abspath(__file__))
 
-ws = gams.GamsWorkspace(working_directory=this_dir, system_directory=gamspy_base_dir)
+    gamspy_base_dir = gamspy_base.__path__[0]
 
-model = ws.add_job_from_file("farmer_linear_augmented.gms")
+    ws = gams.GamsWorkspace(working_directory=this_dir, system_directory=gamspy_base_dir)
 
-model.run(output=sys.stdout)
+    job = ws.add_job_from_file("farmer_linear_augmented.gms")
+
+    job.run(output=sys.stdout)
+
+    cp = ws.add_checkpoint()
+    mi = cp.add_modelinstance()
+
+    job.run(checkpoint=cp)
+
+    crop = mi.sync_db.add_set("crop", 1, "crop type")
+
+    y = mi.sync_db.add_parameter_dc("yield", [crop,], "tons per acre")
+
+    ph_W = mi.sync_db.add_parameter_dc("ph_W", [crop,], "ph weight")
+    xbar = mi.sync_db.add_parameter_dc("xbar", [crop,], "ph average")
+    rho = mi.sync_db.add_parameter_dc("rho", [crop,], "ph rho")
+
+    W_on = mi.sync_db.add_parameter("W_on", 0, "activate w term")
+    prox_on = mi.sync_db.add_parameter("prox_on", 0, "activate prox term")
+
+    mi.instantiate("simple min negprofit using lp",
+        [
+            gams.GamsModifier(y),
+            gams.GamsModifier(ph_W),
+            gams.GamsModifier(xbar),
+            gams.GamsModifier(rho),
+            gams.GamsModifier(W_on),
+            gams.GamsModifier(prox_on),
+        ],
+    )
+
+    # initialize W, rho, xbar, W_on, prox_on
+    crops = [ "wheat", "corn", "sugarbeets" ]
+    for c in crops:
+        ph_W.add_record(c).value = 0
+        xbar.add_record(c).value = 0
+        rho.add_record(c).value = 0
+    W_on.add_record().value = 0
+    prox_on.add_record().value = 0
+
+    # scenario specific data applied
+    assert scennum < 3, "three scenarios hardwired for now"
+    if scennum == 0:  # below
+        y.add_record("wheat").value = 2.0
+        y.add_record("corn").value = 2.4
+        y.add_record("sugarbeets").value = 16.0
+    elif scennum == 1: # average
+        y.add_record("wheat").value = 2.5
+        y.add_record("corn").value = 3.0
+        y.add_record("sugarbeets").value = 20.0
+    elif scennum == 2: # above
+        y.add_record("wheat").value = 3.0
+        y.add_record("corn").value = 3.6
+        y.add_record("sugarbeets").value = 24.0
+
+    mi.solve()
+    areaVarDatas = list( mi.sync_db["x"] )
+
+    print(f"after simple solve for scenario {scennum}")
+    for i,v in enumerate(areaVarDatas):
+        print(f"{i =}, {v.get_level() =}")
+
+    # In general, be sure to process variables in the same order has the guest does (so indexes match)
+    gd = {
+        "scenario": mi,
+        "nonants": {("ROOT",i): v for i,v in enumerate(areaVarDatas)},
+        "nonant_fixedness": {("ROOT",i): v.get_lower() == v.get_upper() for i,v in enumerate(areaVarDatas)},
+        "nonant_start": {("ROOT",i): v.get_level() for i,v in enumerate(areaVarDatas)},
+        "nonant_names": {("ROOT",i): ("x", v.key(0)) for i, v in enumerate(areaVarDatas)},
+        "probability": "uniform",
+        "sense": None,
+        "BFs": None,
+        "ph" : {
+            "ph_W" : {("ROOT",i): p for i,p in enumerate(ph_W)},
+            "xbar" : {("ROOT",i): p for i,p in enumerate(xbar)},
+            "rho" : {("ROOT",i): p for i,p in enumerate(rho)},
+            "W_on" : W_on.first_record(),
+            "prox_on" : prox_on.first_record(),
+            "obj" : mi.sync_db["negprofit"].find_record(),
+            "nonant_lbs" : {("ROOT",i): v.get_lower() for i,v in enumerate(areaVarDatas)},
+            "nonant_ubs" : {("ROOT",i): v.get_upper() for i,v in enumerate(areaVarDatas)},
+        },
+    }
+
+    return gd
+        
+
+if __name__ == "__main__":
+
+    scennum = 2
+    gd = create_model(scennum=scennum)
+    mi = gd["scenario"]
+    mi.solve()
+    print(f"after solve in main for scenario {scennum}")
+    for ndn_i,gxvar in gd["nonants"].items():
+        print(f"{ndn_i =}, {gxvar.get_level() =}")
+    
