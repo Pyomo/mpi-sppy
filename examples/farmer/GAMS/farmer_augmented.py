460ce38ac4444d9bfd70ff43bbfdddfd46645591
Aymeric Legros <aymeric.legros11@gmail.com>
2024-06-25 09:45:21 -0700

    WIP easier to test things, but name not adapted

--- examples/farmer/GAMS/farmer_augmented.py
+++ examples/farmer/GAMS/farmer_augmented.py
@@ -2,6 +2,7 @@ import os
 import sys
 import gams
 import gamspy_base
+import examples.farmer.farmer_gams_gen_agnostic as farmer_gams_gen_agnostic
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,6 +10,12 @@ gamspy_base_dir = gamspy_base.__path__[0]
 
 ws = gams.GamsWorkspace(working_directory=this_dir, system_directory=gamspy_base_dir)
 
-model = ws.add_job_from_file("farmer_augmented.gms")
+original_file = "farmer_average.gms"
+nonants = "crop"
+farmer_gams_gen_agnostic.create_ph_model(original_file, nonants)
+
+model = ws.add_job_from_file("farmer_average_ph")
+#model = ws.add_job_from_file("farmer_average_completed")
+#model = ws.add_job_from_file("farmer_linear_augmented")
 
 model.run(output=sys.stdout)
