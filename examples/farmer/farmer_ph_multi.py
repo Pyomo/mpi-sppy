cdce21eb7e0ff1d1ab3cedae8a17f2d7d2b207dc
xiaopeng <lxp980713@126.com>
2024-07-03 15:45:16 -0400

    farmer taking different options from terminal input, using smooth_type and allowing pratio

--- examples/farmer/farmer_ph_multi.py
+++ examples/farmer/farmer_ph_multi.py
@@ -2,26 +2,45 @@ from mpisppy.opt.ph import PH
 from mpisppy.convergers.primal_dual_converger import PrimalDualConverger
 import farmer
 from mpisppy.extensions.xhatclosest import XhatClosest
+import sys
 
 
 def main():
-    num_scen = 3
-    crops_multiplier = 1
+    if len(sys.argv) != 6 and len(sys.argv) != 9:
+        print("usage python farmer_ph_multi.py {crops_multiplier} {scen_count} {rho} {itermax} {solver_name}")
+        print("e.g., python farmer_ph_multi.py 1 3 1 300 xpress")
+        print("or python farmer_ph_multi.py {crops_multiplier} {scen_count} {rho} {itermax} {smooth_type} {pvalue_or_pratio} {beta} {solver_name}")
+        print("e.g., python farmer_ph_multi.py 1 3 1 300 1 0.1 0.1 xpress")
+        print("Your length:", len(sys.argv))
+        quit()
+    crops_multiplier = int(sys.argv[1])
+    num_scen = int(sys.argv[2])
+    rho = float(sys.argv[3])
+    itermax = int(sys.argv[4])
+    solver_name = sys.argv[-1]
+    if len(sys.argv) == 9:
+        smooth_type = int(sys.argv[5])
+        pvalue = float(sys.argv[6])
+        beta = float(sys.argv[7])
+    elif len(sys.argv) == 6:
+        smooth_type = 0
+        pvalue = 0.0
+        beta = 0.0
     options = {
-        "solver_name": "xpress_direct",
-        "PHIterLimit": 300,
-        "defaultPHrho": 1,
+        "solver_name": solver_name,
+        "PHIterLimit": itermax,
+        "defaultPHrho": rho,
         "convthresh": -1e-8,
-        "verbose": False,
+        "verbose": True,
         "display_progress": True,
         "display_timing": True,
         "iter0_solver_options": dict(),
         "iterk_solver_options": dict(),
         "display_convergence_detail": True,
-        "smoothed": True,
-        "defaultPHp": .1,
-        "defaultPHbeta": 0.1,
-        "primal_dual_converger_options" : {"tol" : 1e-6},
+        "smoothed": smooth_type,
+        "defaultPHp": pvalue,
+        "defaultPHbeta": beta,
+        "primal_dual_converger_options" : {"tol" : 1e-4},
         'xhat_closest_options': {'xhat_solver_options': {}, 'keep_solution':True}
     }
     scenario_creator = farmer.scenario_creator
