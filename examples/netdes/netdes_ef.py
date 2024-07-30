6fcf219974b032c910e6c43f9d05ebfd06c4ff3f
xiaopeng <lxp980713@126.com>
2024-07-30 00:28:01 -0400

    fix script solving extensive form of netdes problem

--- examples/netdes/netdes_ef.py
+++ examples/netdes/netdes_ef.py
@@ -5,9 +5,17 @@
 from mpisppy.opt.ef import ExtensiveForm
 from netdes import scenario_creator
 import pyomo.environ as pyo
+import sys
 
 def main():
-    inst = "network-10-20-L-01"
+    # inst = "network-10-20-L-01"
+    if len(sys.argv) == 1:
+        inst = "network-10-20-L-01"
+    elif len(sys.argv) == 2:
+        inst = sys.argv[1]
+    else:
+        print("Invalid input.")
+        quit()
     num_scen = int(inst.split("-")[-3])
     scenario_names = [f"Scen{i}" for i in range(num_scen)]
     path = f"./data/{inst}.dat"
@@ -19,7 +27,7 @@ def main():
         model_name=f"{inst}-EF",
         scenario_creator_kwargs={"path": path},
     )
-    results = ef.solve_extensive_form()
+    results = ef.solve_extensive_form(tee=True)
     print("Netdes objective value:", pyo.value(ef.ef.EF_Obj))
 
 if __name__=="__main__":
