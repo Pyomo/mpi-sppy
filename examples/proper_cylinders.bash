4e6c9d59c758665536e24142347d25b13e00bfab
David L. Woodruff <dlwoodruff@ucdavis.edu>
2024-10-01 18:22:32 -0700

    [WIP] just getting started on trying to write bundles

--- examples/proper_cylinders.bash
+++ examples/proper_cylinders.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+SOLVER=cplex
+
 # Note: you still need to name a module to get help
 echo "^^^ help ^^^"
 python -m mpi4py ../mpisppy/proper_cylinders.py --module-name farmer/farmer --help
+
+echo "^^^ write pickle bundles ^^^"
+cd sslp
+python ../../mpisppy/proper_cylinders.py --module-name sslp --sslp-data-path ./data --instance-name sslp_15_45_10 --pickle-bundles-dir sslp_pickles --scenarios-per-bundle 5 --default-rho 1
+cd ..
