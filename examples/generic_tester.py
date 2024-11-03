776ab53c703cfb3e0744cbc948acc1aa58f9e0af
David L. Woodruff <dlwoodruff@ucdavis.edu>
2024-11-02 17:07:12 -0700

    the generic tester should now work with cplex community edition

--- examples/generic_tester.py
+++ examples/generic_tester.py
@@ -168,7 +168,7 @@ sslp_pb = ("--sslp-data-path ./data --instance-name sslp_15_45_10 "
            "--scenarios-per-bundle 1 --default-rho 1 "
            f"--solver-name {solver_name} --max-iterations 5 --lagrangian "
            "--xhatshuffle --rel-gap 0.001")
-rebaseline_xhat("sslp", "sslp", 3, sslp_pb, "test_data/sslp_pb_baseline")
+#rebaseline_xhat("sslp", "sslp", 3, sslp_pb, "test_data/sslp_pb_baseline")
 do_one("sslp", "sslp", 3, sslp_pb, xhat_baseline_dir="test_data/sslp_pb_baseline")
 
 # write, then read, pickle bundles
@@ -178,7 +178,7 @@ sslp_rd = ("--sslp-data-path ./data --instance-name sslp_15_45_10 "
            "--unpickle-bundles-dir sslp_pickles --scenarios-per-bundle 1 "
            f"--default-rho 1 --solver-name={solver_name} "
            "--max-iterations 5 --lagrangian --xhatshuffle --rel-gap 0.001")
-rebaseline_xhat("sslp", "sslp", 3, sslp_rd, "test_data/sslp_rd_baseline")
+#rebaseline_xhat("sslp", "sslp", 3, sslp_rd, "test_data/sslp_rd_baseline")
 do_one("sslp", "sslp", 3, sslp_rd, xhat_baseline_dir="test_data/sslp_rd_baseline")
 
 
