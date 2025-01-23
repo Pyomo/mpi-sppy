2ffd6dc46769df6f58fa70bf1777339546eff71b
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-01-23 09:33:41 -0700

    performance improvements for xhatshuffle

--- mpisppy/cylinders/xhatshufflelooper_bounder.py
+++ mpisppy/cylinders/xhatshufflelooper_bounder.py
@@ -117,7 +117,7 @@ class XhatShuffleInnerBound(spoke.InnerBoundNonantSpoke):
                                          self.iter_step)
 
         def _vb(msg): 
-            if self.verbose and self.opt.cylinder_rank == 0:
+            if True and self.opt.cylinder_rank == 0:
                 print("(rank0) " + msg)
 
         xh_iter = 1
@@ -147,6 +147,18 @@ class XhatShuffleInnerBound(spoke.InnerBoundNonantSpoke):
                 _vb("   Begin epoch")
                 scenario_cycler.begin_epoch()
 
+                # always try at least two for each set of nonants
+                # so we continue the explore the scenarios and
+                # do not stall out on a single scenario because
+                # the hub is moving very fast
+                next_scendict = scenario_cycler.get_next()
+                if next_scendict is not None:
+                    _vb(f"   Trying next {next_scendict}")
+                    update = self.try_scenario_dict(next_scendict)
+                    if update:
+                        _vb(f"   Updating best to {next_scendict}")
+                        scenario_cycler.best = next_scendict["ROOT"]
+
             next_scendict = scenario_cycler.get_next()
             if next_scendict is not None:
                 _vb(f"   Trying next {next_scendict}")
@@ -166,13 +178,14 @@ class ScenarioCycler:
         root_kids = nonleaves['ROOT'].kids if 'ROOT' in nonleaves else None
         if root_kids is None or len(root_kids)==0 or root_kids[0].is_leaf:
             self._multi = False
-            self._iter_shift = 1 if iter_step is None else iter_step
+            self._iter_shift = 0 if iter_step is None else iter_step
             self._use_reverse = False #It is useless to reverse for 2stage SP
         else:
             self._multi = True
             self.BF0 = len(root_kids)
             self._nonleaves = nonleaves
-            
+            # TODO: is this right for multistage, or should the default be
+            #       0 like in the two-stage case?
             self._iter_shift = self.BF0 if iter_step is None else iter_step
             self._use_reverse = True if reverse is None else reverse
             self._reversed = False #Do we iter in reverse mode ?
@@ -215,7 +228,7 @@ class ScenarioCycler:
             filling_idx +=1
             filling_idx %= self._num_scenarios
         
-    def create_nodescen_dict(self):
+    def _create_nodescen_dict(self):
         '''
         Creates an attribute nodescen_dict. 
         Keys are nonleaf names, values are local scenario names 
@@ -229,7 +242,7 @@ class ScenarioCycler:
             self.nodescen_dict = dict()
             self._fill_nodescen_dict(self._nonleaves.keys())
     
-    def update_nodescen_dict(self,snames_to_remove):
+    def _update_nodescen_dict(self,snames_to_remove):
         '''
         WARNING: _cur_ROOTscen must be up to date when calling this method
         '''
@@ -256,7 +269,7 @@ class ScenarioCycler:
         self._shuffled_snames = [s[1] for s in self._shuffled_scenarios]
         self._original_order = [s[0] for s in self._shuffled_scenarios]
         self._cur_ROOTscen = self._shuffled_snames[0] if self.best is None else self.best
-        self.create_nodescen_dict()
+        self._create_nodescen_dict()
         
         self._scenarios_this_epoch = set()
     
@@ -265,7 +278,7 @@ class ScenarioCycler:
         self._shuffled_snames = [s[1] for s in reversed(self._shuffled_scenarios)]
         self._original_order = [s[0] for s in reversed(self._shuffled_scenarios)]
         self._cur_ROOTscen = self._shuffled_snames[0] if self.best is None else self.best
-        self.create_nodescen_dict()
+        self._create_nodescen_dict()
         
         self._scenarios_this_epoch = set()
 
@@ -296,9 +309,9 @@ class ScenarioCycler:
         #Updating scenarios
         self._cur_ROOTscen = self._shuffled_snames[self._cycle_idx]
         if old_idx<self._cycle_idx:
-            scens_to_remove = self._shuffled_snames[old_idx:self._cycle_idx]
+            scens_to_remove = set(self._shuffled_snames[old_idx:self._cycle_idx])
         else:
-            scens_to_remove = self._shuffled_snames[old_idx:]+self._shuffled_snames[:self._cycle_idx]
-        self.update_nodescen_dict(scens_to_remove)
+            scens_to_remove = set(self._shuffled_snames[old_idx:]+self._shuffled_snames[:self._cycle_idx])
+        self._update_nodescen_dict(scens_to_remove)
         
     
