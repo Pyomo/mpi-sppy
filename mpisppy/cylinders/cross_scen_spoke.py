94fcd2a09ff6dbcd379ede0a89ce07b270ba7631
Jonathan Maack <jonathan.maack@nrel.gov>
2025-01-23 15:37:00 -0700

    Remove unused import

--- mpisppy/cylinders/cross_scen_spoke.py
+++ mpisppy/cylinders/cross_scen_spoke.py
@@ -10,7 +10,6 @@
 # TODO Remove after this program no longer support Python 3.8
 from __future__ import annotations
 
-from mpisppy.cylinders.spcommunicator import communicator_array
 from pyomo.repn.standard_repn import generate_standard_repn
 from mpisppy import MPI
 from mpisppy.utils.lshaped_cuts import LShapedCutGenerator
