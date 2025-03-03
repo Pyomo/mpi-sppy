a96a3f6c506dc75a7921fc965611afe804183097
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-03-03 15:43:07 -0700

    not so restrictive rc_fixer

--- mpisppy/utils/config.py
+++ mpisppy/utils/config.py
@@ -577,9 +577,9 @@ class Config(pyofig.ConfigDict):
         
         self.add_to_config('rc_fixer_require_improving_lagrangian',
                             description="Only consider fixing / unfixing variables after the lagrangian "
-                                        "bound computed by the reduced cost spoke has improved. (default True)",
+                                        "bound computed by the reduced cost spoke has improved. (default False)",
                             domain=bool,
-                            default=True)
+                            default=False)
 
         self.add_to_config('rc_zero_tol',
                             description="vars with rc below tol will never be fixed",
