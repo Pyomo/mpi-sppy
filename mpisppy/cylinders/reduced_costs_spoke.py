b75be2c899783fc0d42d4b08ac5b4242c798d4c1
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-04-08 09:02:14 -0600

    update_nonant_bounds at every calculation

--- mpisppy/cylinders/reduced_costs_spoke.py
+++ mpisppy/cylinders/reduced_costs_spoke.py
@@ -317,6 +317,8 @@ class ReducedCostsSpoke(LagrangianOuterBound):
             Field.NONANT_UPPER_BOUNDS,
         )
 
+        self.update_nonant_bounds()
+
     def update_nonant_bounds(self):
         bounds_modified = 0
         send_buf = self.send_buffers[Field.NONANT_LOWER_BOUNDS]
@@ -350,7 +352,6 @@ class ReducedCostsSpoke(LagrangianOuterBound):
         super().do_while_waiting_for_new_Ws(need_solution=need_solution, warmstart=False)
         # might as well see if a tighter upper bound has come along
         self.extract_and_store_updated_nonant_bounds(new_dual=False)
-        self.update_nonant_bounds()
 
     def main(self):
         # need the solution for ReducedCostsSpoke
