f82d96b6b0da5097e75700e0c6407c48622b8774
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-10-24 12:40:42 -0600

    NFC: header for colmap.py

--- examples/loose_agnostic/GAMS/colmap.py
+++ examples/loose_agnostic/GAMS/colmap.py
@@ -1,3 +1,11 @@
+###############################################################################
+# mpi-sppy: MPI-based Stochastic Programming in PYthon
+#
+# Copyright (c) 2025, Lawrence Livermore National Security, LLC, Alliance for
+# Sustainable Energy, LLC, The Regents of the University of California, et al.
+# All rights reserved. Please see the files COPYRIGHT.md and LICENSE.md for
+# full copyright and license information.
+###############################################################################
 #!/usr/bin/env python3
 """
 Map (xN,value) CSV to (original GAMS var name,value) using GAMS CONVERT dict.txt.
