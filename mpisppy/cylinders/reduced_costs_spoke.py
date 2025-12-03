a1bae4096ed26653075236e09f90a550e347e158
Bernard Knueven <Bernard.Knueven@nrel.gov>
2025-12-02 20:51:58 -0700

    Revert "Merge pull request #531 from bknueven/rc_spoke_enhancement"
    This reverts commit 285c2faf83e0141a8b715dc70c22743e3a6071d4, reversing
    changes made to c9eaca8fb0ad723c14f9a30636575f52a05b4a42.

--- mpisppy/cylinders/reduced_costs_spoke.py
+++ mpisppy/cylinders/reduced_costs_spoke.py
@@ -139,8 +139,6 @@ class ReducedCostsSpoke(LagrangianOuterBound):
             self.extract_and_store_reduced_costs()
             self.update_bounding_functions(bound)
             self.extract_and_store_updated_nonant_bounds(new_dual=True)
-        else:
-            self.extract_and_store_updated_nonant_bounds(new_dual=False)
         return bound
 
     def extract_and_store_reduced_costs(self):
@@ -319,8 +317,6 @@ class ReducedCostsSpoke(LagrangianOuterBound):
             Field.NONANT_UPPER_BOUNDS,
         )
 
-        self.update_nonant_bounds()
-
     def update_nonant_bounds(self):
         bounds_modified = 0
         send_buf = self.send_buffers[Field.NONANT_LOWER_BOUNDS]
@@ -354,6 +350,7 @@ class ReducedCostsSpoke(LagrangianOuterBound):
         super().do_while_waiting_for_new_Ws(need_solution=need_solution, warmstart=False)
         # might as well see if a tighter upper bound has come along
         self.extract_and_store_updated_nonant_bounds(new_dual=False)
+        self.update_nonant_bounds()
 
     def main(self):
         # need the solution for ReducedCostsSpoke
