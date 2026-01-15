d565f26341c4c982afb213af72e30a2209fc1ad1
Moritz Schreiber <68053396+mosc9575@users.noreply.github.com>
2026-01-15 14:41:27 +0100

    define scenario_names_creator as a utility function and add tests

---
+++ mpisppy/tests/test_scenario_names_creator.py
@@ -0,0 +1,50 @@
+###############################################################################
+# mpi-sppy: MPI-based Stochastic Programming in PYthon
+#
+# Copyright (c) 2024, Lawrence Livermore National Security, LLC, Alliance for
+# Sustainable Energy, LLC, The Regents of the University of California, et al.
+# All rights reserved. Please see the files COPYRIGHT.md and LICENSE.md for
+# full copyright and license information.
+###############################################################################
+
+from unittest import TestCase
+
+from mpisppy.utils import scenario_names_creator
+
+
+class TestScenarioNamesCeator(TestCase):
+    def test_scenario_names_creator_default(self):
+        self.assertEqual(
+            scenario_names_creator(3),
+            ["scenario0", "scenario1", "scenario2"],
+            "Should be a list with 'scenario0', 'scenario1' and 'scenario2'",
+        )
+
+    def test_scenario_names_creator_defining_prefix(self):
+        self.assertEqual(
+            scenario_names_creator(3, prefix="s"),
+            ["s0", "s1", "s2"],
+            "Should be a list with 's0', 's1' and 's2'",
+        )
+
+    def test_scenario_names_creator_defining_seperator(self):
+        self.assertEqual(
+            scenario_names_creator(3, separator="_"),
+            ["scenario_0", "scenario_1", "scenario_2"],
+            "Should be a list with 'scenario_0', 'scenario_1' and 'scenario_2'",
+        )
+
+    def test_scenario_names_creator_setting_start_value(self):
+        self.assertEqual(
+            scenario_names_creator(3, start=1),
+            ["scenario1", "scenario2", "scenario3"],
+            "Should be a list with 'scenario1', 'scenario2' and 'scenario3'",
+        )
+
+    def test_scenario_names_creator_start_value_is_None(self):
+        # to avoid migration errors from earlier implementations start is also allowed to be None
+        self.assertEqual(
+            scenario_names_creator(3, start=None),
+            ["scenario0", "scenario1", "scenario2"],
+            "Should be a list with 'scenario10', 'scenario1' and 'scenario2'",
+        )
--- mpisppy/utils/strings.py
+++ mpisppy/utils/strings.py
@@ -45,3 +45,28 @@ def nice_join(
         return separator.join(seq)
     else:
         return f"{separator.join(seq[:-1])} {conjunction} {seq[-1]}"
+
+
+def scenario_names_creator(
+    n: int, prefix: str = "scenario", separator: str = "", start: int | None = 0
+) -> list[str]:
+    """Creates a list of scenario names using the pattern `{prefix}{separator}{number}` for a single
+    scenario name. The scenario names are distinguished using consecutive numbers.
+
+    Args:
+        n (int): number of wanted scenarios names
+        prefix (str, "scenario"): string to start the scenario name
+        separator (str, ""): string to separate the prefix from the consecutive number
+        start (int | None, 0): number to use for the first scenario name
+
+    Examples:
+
+        >>> from mpisppy.utils import scenario_names_creator
+
+        >>> scenario_names_creator(3, prefix="scen", separator="_", start=1)
+        ["scen_1", "scen_2", "scen_3"]
+    """
+    # to avoid migration errors from earlier implementations start is also allowed to be None
+    if start is None:
+        start = 0
+    return [f"{prefix}{separator}{i}" for i in range(start, start + n)]
