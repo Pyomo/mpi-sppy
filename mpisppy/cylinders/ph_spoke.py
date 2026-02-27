aa610d58b70854e3e31bbeb6ca65ad88ab0de2dc
Flores Quiroz <floresquiroz1@llnl.gov>
2026-02-27 11:06:33 -0800

    correcting headers

--- mpisppy/cylinders/ph_spoke.py
+++ mpisppy/cylinders/ph_spoke.py
@@ -1,3 +1,12 @@
+###############################################################################
+# mpi-sppy: MPI-based Stochastic Programming in PYthon
+#
+# Copyright (c) 2024, Lawrence Livermore National Security, LLC, Alliance for
+# Sustainable Energy, LLC, The Regents of the University of California, et al.
+# All rights reserved. Please see the files COPYRIGHT.md and LICENSE.md for
+# full copyright and license information.
+###############################################################################
+
 from mpisppy.cylinders.spoke import Spoke, Field
 import pyomo.environ as pyo
 
