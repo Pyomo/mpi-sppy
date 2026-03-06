c66423a529748ac6761117e9cfda0c181952cd24
bknueven <30801372+bknueven@users.noreply.github.com>
2026-03-06 12:01:28 -0700

    NFC: remove out-of-date comment.

--- mpisppy/phbase.py
+++ mpisppy/phbase.py
@@ -624,7 +624,6 @@ class PHBase(mpisppy.spopt.SPOpt):
         update proximal term approximation by potentially
         adding a linear cut near each current xvar value
 
-        NOTE: This is expensive, but works
         """
         tol = self.prox_approx_tol
         for sn, s in self.local_scenarios.items():
