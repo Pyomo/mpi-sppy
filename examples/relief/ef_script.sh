ed3868ac33931f88ae9c251575b6da66c4c5630b
Tomas <valenciazulu1@llnl.gov>
2026-03-26 17:03:16 -0700

    adding relief disaster example

---
+++ examples/relief/ef_script.sh
@@ -0,0 +1,1 @@
+python -m mpisppy.generic_cylinders --module-name relief --num-scens 3 --EF --EF-solver-name gurobi --tee-EF
---
+++ examples/relief/lshaped_script.sh
@@ -0,0 +1,1 @@
+mpiexec -n 6 python -m mpisppy.generic_cylinders --module-name relief --num-scens 3 --lshaped-hub --solver-name gurobi --xhatlshaped --max-iterations 100 --rel-gap 1e-4
